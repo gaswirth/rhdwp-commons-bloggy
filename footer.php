e81e1076c48035576e3e60606f26fb11e3657b4a
Nick Gaswirth <nick@roundhouse-designs.com>
2015-04-30 10:06:28 -0400

    Tweaks

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 
     <footer id="colophon" role="contentinfo">
         <?php get_sidebar( 'footer' ); ?>
-        <div class="site-info">
+        <div id="site-info">
 			<p>
 				<?php echo '&copy;' . date( 'Y' ); ?> <?php echo bloginfo( 'name' ); ?> <?php echo ( rhd_is_mobile() ) ? '<br>' : '&bull; '; ?>Site by <a href="//roundhouse-designs.com" target="_blank">Roundhouse<img id="rhd-logo-footer" src="//assets.roundhouse-designs.com/images/rhd-white-house.png" alt="Roundhouse Designs">Designs</a>
             </p>
