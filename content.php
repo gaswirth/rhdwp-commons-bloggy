d1081fb41ec221adb4b46eb8423671280409df28
Nick Gaswirth <nick@roundhouse-designs.com>
2015-04-30 17:12:35 -0400

    h1 to h2

--- content.php
+++ content.php
@@ -10,11 +10,11 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<?php if ( is_single() ) : ?>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<h2 class="entry-title"><?php the_title(); ?></h2>
 			<?php else : ?>
-				<h1 class="entry-title">
+				<h2 class="entry-title">
 					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'rhd' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h1>
+				</h2>
 			<?php endif; // is_single() ?>
 		</header><!-- .entry-header -->
 
@@ -26,11 +26,11 @@
 			<div class="entry-content">
 				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'rhd' ) ); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'rhd' ), 'after' => '</div>' ) ); ?>
-				
+
 			</div><!-- .entry-content -->
 		<?php endif; ?>
 
 		<footer class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'rhd' ), '<span class="edit-link">', '</span>' ); ?>				
+			<?php edit_post_link( __( 'Edit', 'rhd' ), '<span class="edit-link">', '</span>' ); ?>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
